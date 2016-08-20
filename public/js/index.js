c73907d74138dfdc646149b617bc99d4df836347
SmileDandelion <ning.chn@foxmail.com>
2016-08-20 23:06:11 +0800

    用redux初始化store和reducer

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-class App extends Component{
-    render(){
+import {render} from 'react-dom';
+import {createStore} from 'redux';
+import reducer from './reducers/reducer';
+
+const store = createStore(reducer);
+
+class App extends Component {
+    render() {
         return <div>
             hello
         </div>
     }
 }
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+function renderFunction() {
+    render(<App/>, document.getElementById('root'));
+}
+
+//注册监听器
+store.subscribe(renderFunction);
+
+renderFunction();
