cc30ba08fffe553eaddb11d4a72e8c5461ebd647
SmileDandelion <ning.chn@foxmail.com>
2016-08-21 02:22:30 +0800

    初始化项目用react-redux

--- public/js/index.js
+++ public/js/index.js
@@ -1,47 +1,19 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import {createStore} from 'redux';
-
-import AddTodo from './components/addtodo';
-import TodoList from './components/todolist';
-import Footer from './components/footer';
 import reducer from './reducers/reducer';
-
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
 const store = createStore(reducer);
 
-
 class App extends Component {
-    add(text) {
-        store.dispatch({type: 'ADD', text});
-    }
-
-    toggle(index) {
-        store.dispatch({type: 'TOGGLE', index});
-    }
-
-    delete(index) {
-        store.dispatch({type: 'DELETE', index});
-    }
-
-    filter(filterName){
-        store.dispatch({type:'SET_FILTER',filterName});
-    }
     render() {
         return <div>
-            <AddTodo onAdd={this.add.bind(this)}/>
-            <TodoList onDelete={this.delete.bind(this)} onToggle={this.toggle.bind(this)}
-                      todos={store.getState().filter}/>
-            <Footer onFilter={this.filter.bind(this)}/>
+            hello
         </div>
     }
 }
 
-function renderFunction() {
-    render(<App/>, document.getElementById('root'));
-}
-
-//注册监听器
-store.subscribe(renderFunction);
-
-renderFunction();
+render(<Provider store={store}>
+    <App/>
+</Provider>, document.getElementById('root'));
--- public/js/reducers/reducer.js
+++ public/js/reducers/reducer.js
@@ -1,24 +1,3 @@
-export default function reducer(state = {todos: [], filter: []}, action) {
-    switch (action.type) {
-        case 'ADD':
-            state.todos.push({text: action.text, isDone: false});
-            state.filter = state.todos;
-            return state;
-        case 'TOGGLE':
-            state.todos[action.index].isDone = !state.todos[action.index].isDone;
-            return state;
-        case 'DELETE':
-            state.todos.splice(action.index, 1);
-            return {todos: state.todos,filter:state.todos};
-        case 'SET_FILTER':
-            if (action.filterName === 'ALL') {
-                state.filter = state.todos;
-            } else if (action.filterName === 'ACTIVED') {
-                state.filter = state.todos.filter(todo=>!todo.isDone);
-            } else if (action.filterName === 'COMPLETED') {
-                state.filter = state.todos.filter(todo=>todo.isDone);
-            }
-            return state;
-    }
+export default function reducer(state, action) {
     return state;
 }
